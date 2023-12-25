458074331612293a9db2e44b0e03186f2346e972
tony lua <tonylua@tonydeMacBook-Pro.local>
2023-12-25 09:57:10 +0800

    dump & unit

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './dump-components/HelloWorld.vue'
 import { Color } from '@domains/Enum'
 </script>
 
